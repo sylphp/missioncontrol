392a4c55ff7cd044ceb3031208ba88437c9cf595
lsecasas <louie.casas@outlook.com>
2016-10-05 08:58:29 +0000

    created superuser, added mission model class, and connected class to admin

--- missions/models.py
+++ missions/models.py
@@ -1,5 +1,26 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Mission(models.Model):
+    client = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    details = models.TextField()
+    deliverables = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    selected_date = models.DateTimeField(
+            blank=True, null=True)
+    approved_date = models.DateTimeField(
+            blank=True, null=True)
+    finished_date = models.DateTimeField(
+            blank=True, null=True)
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+    
+    def __str__(self):
+        return self.title
